7a1348aa25ba4f4cfef405242b56624e59cfffd1
sandeepa97 <lbsandeepa@gmail.com>
2021-09-02 18:45:13 +0530

    basics

---
+++ day_1/DemoVariable.java
@@ -0,0 +1,27 @@
+class DemoVariable{
+	public static void main(String args[]){
+		
+		System.out.println("-----Variables-----");
+		int i = 25;
+		System.out.println(i);
+		char myChar = 'A';
+		System.out.println(myChar);
+		boolean b = true;
+		System.out.println(b);
+		byte myB = 5;
+		System.out.println(myB);
+		long myL = 1000L;
+		System.out.println(myL);
+		float f1 = 235.5f;
+		System.out.println(f1);
+		double myD = 153.2;
+		System.out.println(myD);
+		
+		String fname = "Sandeepa";
+		String lname = "Loku";
+		String sp = " ";
+		System.out.print(fname + sp +lname);
+		// System.out.println(lname);
+	}
+
+}
---
+++ day_1/SriLanka.java
@@ -0,0 +1,8 @@
+class SriLanka{
+	public static void main(String args[]){
+		/* System.out.println("Hello Sri Lanka");
+		System.out.print("Negombo"); */
+		System.out.println("Colombo");
+	}
+
+}
